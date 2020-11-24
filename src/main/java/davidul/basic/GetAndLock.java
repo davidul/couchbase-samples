cbe752393c83a07492157de9b3ec5f9d61cb0375
DAVIDUL <ulicny.david@gmail.com>
2020-11-24 06:43:49 +0100

    Get and Lock

--- src/main/java/davidul/basic/GetAndLock.java
+++ src/main/java/davidul/basic/GetAndLock.java
@@ -1,7 +1,14 @@
 package davidul.basic;
 
 import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.ReactiveCollection;
+import com.couchbase.client.java.json.JsonObject;
 import com.couchbase.client.java.kv.GetResult;
+import com.couchbase.client.java.kv.ReplaceOptions;
+import com.couchbase.transactions.TransactionDurabilityLevel;
+import com.couchbase.transactions.Transactions;
+import com.couchbase.transactions.config.TransactionConfigBuilder;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -10,5 +17,22 @@ public class GetAndLock {
     public GetResult getAndLock(String connectionString, String id){
         final Collection collection = CouchbaseConnection.collection(connectionString);
         return collection.getAndLock(id, Duration.ofSeconds(20));
+    }
+
+    public Mono<GetResult> getAndLockReactive(String connectionString, String id){
+        final ReactiveCollection reactive = CouchbaseConnection.collection(connectionString).reactive();
+        return reactive.getAndLock(id, Duration.ofSeconds(20));
+    }
+
+    public void getAndLockInTrx(String connectionString, String id){
+        final Collection collection = CouchbaseConnection.collection(connectionString);
+        final Transactions transactions = Transactions.create(CouchbaseConnection.cluster(connectionString), TransactionConfigBuilder.create().durabilityLevel(TransactionDurabilityLevel.MAJORITY_AND_PERSIST_TO_ACTIVE));
+        transactions.run(ctx -> {
+            final GetResult andLock = collection.getAndLock(id, Duration.ofMillis(1000));
+            final JsonObject jsonObject = andLock.contentAsObject();
+            jsonObject.put("trx", "trx");
+            collection.replace(id, jsonObject, ReplaceOptions.replaceOptions().cas(andLock.cas()));
+            ctx.commit();
+        });
     }
 }
--- src/test/java/davidul/basic/GetAndLockTest.java
+++ src/test/java/davidul/basic/GetAndLockTest.java
@@ -3,14 +3,17 @@ package davidul.basic;
 import com.couchbase.client.java.Collection;
 import com.couchbase.client.java.kv.GetResult;
 import davidul.ContainerSetup;
+import io.vavr.collection.List;
 import org.assertj.core.api.Assertions;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import reactor.core.publisher.Mono;
 
 public class GetAndLockTest {
 
     private static String connectionString;
     private static final String ID_1 = "ID::1";
+    private static final String ID_2 = "ID::2";
 
     @BeforeClass
     public static void setup(){
@@ -23,6 +26,11 @@ public class GetAndLockTest {
         upsert.upsert(connectionString, ID_1);
         final GetAndLock getAndLock = new GetAndLock();
         final GetResult andLock = getAndLock.getAndLock(connectionString, ID_1);
+        /*try {
+            final GetResult andLock1 = getAndLock.getAndLock(connectionString, ID_1);
+        }catch (Exception e){
+            e.printStackTrace();
+        }*/
         final Collection collection = CouchbaseConnection.collection(connectionString);
         final GetResult result = collection.get(ID_1);
 
@@ -32,5 +40,28 @@ public class GetAndLockTest {
 
         Assertions.assertThat(resultUnlocked.cas()).isNotEqualTo(-1);
     }
+
+    @Test
+    public void reactive(){
+        final Upsert upsert = new Upsert();
+        upsert.upsert(connectionString, ID_2);
+        final GetAndLock getAndLock = new GetAndLock();
+        final Mono<GetResult> andLock1 = getAndLock.getAndLockReactive(connectionString, ID_2);
+        final Mono<GetResult> andLock2 = getAndLock.getAndLockReactive(connectionString, ID_2);
+
+        andLock1.subscribe(getResult -> System.out.println("Lock1 " + getResult.cas()), ex -> ex.printStackTrace());
+        andLock2.subscribe(getResult -> System.out.println("Lock2 " + getResult.cas()), ex -> ex.printStackTrace());
+        System.out.println("");
+        final List<Integer> map = List.range(1, 10000000).map(f -> f * f);
+    }
+
+    @Test
+    public void inTrx(){
+        final Upsert upsert = new Upsert();
+        upsert.upsert(connectionString, ID_1);
+        final GetAndLock getAndLock = new GetAndLock();
+        getAndLock.getAndLockInTrx(connectionString, ID_1);
+    }
+
 
 }
