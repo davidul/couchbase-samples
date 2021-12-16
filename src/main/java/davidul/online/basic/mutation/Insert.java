c8bc3f6f6671343ec494201536de597c9426cc93
David Ulicny <ulicny.david@gmail.com>
2021-12-16 14:09:27 +0100

    Reactive insert

--- src/main/java/davidul/online/basic/mutation/Insert.java
+++ src/main/java/davidul/online/basic/mutation/Insert.java
@@ -4,6 +4,7 @@ import com.couchbase.client.java.Collection;
 import com.couchbase.client.java.json.JsonObject;
 import com.couchbase.client.java.kv.MutationResult;
 import davidul.online.connection.SimpleCouchbaseConnection;
+import reactor.core.publisher.Mono;
 
 public class Insert {
     public static MutationResult insert(String connectionString, String documentId, JsonObject object){
@@ -12,5 +13,11 @@ public class Insert {
 
     public static MutationResult insert(Collection collection, String documentId, JsonObject object){
         return collection.insert(documentId, object);
+    }
+
+    public static Mono<MutationResult> insertReactive(String connectionString, String documentId, JsonObject object){
+        return SimpleCouchbaseConnection
+                .reactiveCollection(connectionString)
+                .insert(documentId, object);
     }
 }
--- src/test/java/davidul/online/basic/mutation/InsertTest.java
+++ src/test/java/davidul/online/basic/mutation/InsertTest.java
@@ -8,6 +8,8 @@ import davidul.online.basic.sampledata.SampleData;
 import org.assertj.core.api.Assertions;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static davidul.online.basic.mutation.Upsert.upsert;
 
@@ -16,6 +18,7 @@ public class InsertTest {
     private static String connectionString;
     private static final String ID_1 = "ID::1";
     private static final String ID_2 = "ID::2";
+    private static final String ID_3 = "ID::3";
 
     @BeforeClass
     public static void setup() {
@@ -33,5 +36,15 @@ public class InsertTest {
     @Test(expected = DocumentExistsException.class)
     public void insert_failed(){
         MutationResult insert = Insert.insert(connectionString,ID_1, SampleData.sample());
+    }
+
+    @Test
+    public void insert_reactive(){
+        final Mono<MutationResult> mutationResultMono = Insert.insertReactive(connectionString, ID_3, SampleData.sample());
+
+        StepVerifier
+                .create(mutationResultMono)
+                .expectNextCount(1)
+                .verifyComplete();
     }
 }
