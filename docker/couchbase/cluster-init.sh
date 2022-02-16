088284544ee017cb74c35deb33d9baa743c84280
David Ulicny <ulicny.david@gmail.com>
2022-02-16 16:10:06 +0100

    Cluster init

--- docker/couchbase/cluster-init.sh
+++ docker/couchbase/cluster-init.sh
@@ -11,6 +11,11 @@ check_db() {
   echo $?
 }
 
+check_cluster(){
+  couchbase-cli server-list -c ${MASTER} -u Administrator -p password > /dev/null
+  echo $?
+}
+
 # Variable used in echo
 i=1
 # Echo with
@@ -21,8 +26,9 @@ log() {
 
 until [[ $(check_db) = 0 ]]; do
   >&2 log "Waiting for Couchbase Server to be available ..."
-  sleep 1
+  sleep 5
 done
+
 
 log "Init node"
 couchbase-cli node-init -c ${HOSTNAME} -u Administrator -p password
@@ -30,7 +36,7 @@ couchbase-cli node-init -c ${HOSTNAME} -u Administrator -p password
 if [ -z "$MASTER" ]
  then
    log "Init cluster"
-    couchbase-cli cluster-init -c ${HOSTNAME} --cluster-username Administrator \
+    couchbase-cli cluster-init -c master --cluster-username Administrator \
       --cluster-password password --services data,index,query \
       --cluster-ramsize 512 \
       --cluster-index-ramsize 512 \
@@ -38,18 +44,25 @@ if [ -z "$MASTER" ]
       --index-storage-setting default
 fi
 
-#if [ ! -z "$MASTER" ]
-#  then
-#  couchbase-cli server-add -c ${MASTER} \
-#     --username Administrator \
-#     --password password \
-#     --server-add http://${HOSTNAME}:8091 \
-#     --server-add-username Administrator \
-#     --server-add-password password \
-#     --services data,index,query
-#
-#  couchbase-cli rebalance -c ${HOSTNAME}:8091 --username Administrator --password password
-#fi
+if [ ! -z "$MASTER" ]
+  then
+  until [[ $(check_cluster) = 0 ]]; do
+    >&2 log "Waiting for cluster ..."
+    sleep 1
+  done
+
+  ipaddress=$(ifconfig eth0 | grep -w inet | tr -s ' ' |  cut -d' ' -f3)
+
+  couchbase-cli server-add -c ${MASTER} \
+     --username Administrator \
+     --password password \
+     --server-add http://${ipaddress}:8091 \
+     --server-add-username Administrator \
+     --server-add-password password \
+     --services data,index,query
+
+  couchbase-cli rebalance -c ${HOSTNAME}:8091 --username Administrator --password password
+fi
 
 #couchbase-cli server-add -c 127.0.0.1:8091 --username Administrator --password password --server-add http://172.16.101.4:8091 --server-add-username Administrator --server-add-password password --services data,index,query
 
